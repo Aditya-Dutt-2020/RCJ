9e837ebe0112e15b3506f72a462f11652a785436
MannanB <37980685+MannanB@users.noreply.github.com>
2020-01-12 17:19:20 -0500

    Add files via upload

---
+++ piCode/serialPi.py
@@ -0,0 +1,20 @@
+import serial
+
+# Example Usage
+'''
+import serialPi
+serialPi.initSerial()
+serialPi.write("Go")
+serialPi.write("Stop")
+'''
+
+def initSerial():
+	global s1
+	s1 = serial.Serial('COM6', 115200)
+	s1.flushInput()
+
+#comp_list = [b"Flash Complete\r\n", b"Hello Pi, this is arduino UNO...\r\n"]
+
+def write(code)
+    s1.write(str.encode(code))
+
