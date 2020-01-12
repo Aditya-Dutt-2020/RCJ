d5622b705ebb711347b9659b1e87665b946e5501
MannanB <37980685+MannanB@users.noreply.github.com>
2020-01-12 14:32:01 -0500

    Added some misc files

---
+++ ArduinoCode/main.ino
@@ -0,0 +1,13 @@
+#include "motorSheildControl.h"
+#include "ultrasonic.h"
+#include "serialArduinoTest.h"
+
+void setup(){
+  initUS();
+  initMotorshield();
+  initSerial();
+}
+
+void loop(){
+  
+}
