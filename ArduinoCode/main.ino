a4e19144dc5fc4a31d14dce4b7c065476fe0a16e
MannanB <37980685+MannanB@users.noreply.github.com>
2020-01-14 20:06:36 -0500

    Added serial stuff

--- ArduinoCode/main.ino
+++ ArduinoCode/main.ino
@@ -1,6 +1,9 @@
 #include "motorSheildControl.h"
 #include "ultrasonic.h"
-#include "serialArduinoTest.h"
+#include "serialArduino.h"
+
+bool progStop = false;
+int spd = 175;
 
 void setup(){
   initUS();
@@ -9,5 +12,25 @@ void setup(){
 }
 
 void loop(){
-  
+  movement comm = readPi();
+  if (comm.mag != 997){
+    if (comm.mag == 998){ progStop = true;}
+    else if (comm.mag == 999) { progStop = false; }
+    else if (progStop){
+      if (comm.MoT == 1){
+        pointTurn(comm.dir, comm.mag, spd);
+      }
+      else if (comm.MoT == 0){
+        if (comm.dir == 0){
+          backward(comm.mag, spd)
+        } else {
+          straight(comm.mag, spd)
+        }
+      }
+    }
+  }
+  if (not progStop){
+    // arduino stuff goes here
+    
+  }
 }
